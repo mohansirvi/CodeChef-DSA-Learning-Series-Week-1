a0dda41c346d1dc804e183cf5c8d661382375079
Mohan Lal Sirvi <52498320+rockstarCSE057@users.noreply.github.com>
2020-04-01 08:00:07 +0530

    Create LifeUniverseEarth.cpp

---
+++ LifeUniverseEarth.cpp
@@ -0,0 +1,21 @@
+//Author MOHAN SIRVI
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define fast ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL)
+#define MOD 1000000007
+
+int main()
+{
+    fast;
+    int n,x=1;
+    while(x>0)
+    {
+        cin>>n;
+        if(n==42)
+            break;
+        else
+            cout<<n<<endl;
+    }
+    return 0;
+}
